ee795c2e13ab33f5034915f74333be01aeac3cf9
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-23 12:23:38 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+client.user.setGame(`صيانه بعد التخريب lol`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -27,5 +27,58 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+
+//كود الهيلب الخاص للزوار
+
+client.on('message', message => {
+     if (message.content === "*help") {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField('     **اوامر عامه** ' ,' **الاوامر العامه الخاصه ب الزاور** ')
+.addField('     ** **  ' ,' ** ** ')
+.addField('     ** *avatar **  ' ,' **لتجيب صورة احد كل العليك تكتب الامر وتمنشنو** ')
+.addField('     ** *mhelp **  ' ,' **لعرض اوامر كود الاغاني** ')
+.addField('     ** *support **  ' ,' **لسيرفر الدعم الفني** ')
+.addField('     ** *inv **  ' ,' **لدعوة البوت لسيرفرك** ')
+.addField('     ** *id **  ' ,' **لمعرفة معلوماتك الخاصه** ')
+.addField('     ** *server ** ' ,' **لمعرفة معلومات السيرفر**')
+.addField('     ** *ping **  ' ,' **لمعرفة سرعة البوت**')
+.addField('     ** *bot **  ' ,' **لمعرفة البوت بكم ** ')
+.addField('**لدعوة البوت لسيرفرك ..**' , '**http://cutt.us/P8eIJ**')
+.addField('     **سيرفر الدعم الفني**  ' ,' **https://discord.gg/Mwyp8d8** ')
+ 
+.setColor('RANDOM')
+  message.channel.sendEmbed(embed);
+    }
+});
+
+//هيلب الادمنيه
+
+client.on('message', message => {
+     if (message.content === "*help") {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField('     **اوامر اداريه** ' ,' **الاومر الخاصه ب الادمنيه** ')
+.addField('     ** **  ' ,' ** ** ')
+.addField('     ** *clear **  ' ,' **لمسح جميع الرسايل ال ب الروم** ')
+.addField('     ** *bc **  ' ,' **ل ارسال رسالة لجميع اعضاء السيرفر** ')
+.addField('     ** *send **  ' ,' **ل عمل تصويت ب روم ** ')
+.addField('     ** *mute **  ' ,' **لعمل ميوت كتابي لحد** ')
+.addField('     ** *unmute ** ' ,' **لفك الميوت الكتابي** ')
+.addField('     ** *cchat **  ' ,' **لتقفيل الشات ما احد يقدر يكتب به** ')
+.addField('     ** *uchat **  ' ,' **لفتح الشات تاني** ')
+.addField('     ** **  ' ,' ** ** ')
+.addField('**لدعوة البوت لسيرفرك ..**' , '**http://cutt.us/P8eIJ**')
+.addField('     **سيرفر الدعم الفني**  ' ,' **https://discord.gg/Mwyp8d8** ')
+.setColor('RANDOM')
+  message.channel.sendEmbed(embed);
+    }
+});
+
+//كود...
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);

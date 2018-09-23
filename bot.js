b598228062da513dd436b15f9974a368b4096a16
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-23 13:02:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -715,12 +715,34 @@ msg.delete();
 
 //كود البرودكاست البدون اي شي فقط الكلام
 
-client.on('message', message => {
-    if (message.content.split(' ')[0] == 'nbc')
-       message.guild.members.forEach( member => {
-         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
 
 //كود تغير البلاينج و الصوره والاسم
+
+const adminprefix = "*";
+const devs = ['427054141492297728','ايدي اخويك'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+   
+if (message.content.startsWith(adminprefix + 'ply')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'pic')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else    
+if (message.content.startsWith(adminprefix + 'tw')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+});
 
 
 //none

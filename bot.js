31be44870bb021ae4f3c22554759724747ec4040
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-24 11:15:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2116,6 +2116,78 @@ if(message.channel.type === "dm") return;
  });
 
 
-//none
+//كود الكيك والبان
 
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+ 
+var command = message.content.split(" ")[0];
+command = command.slice(prefix.length);
+ 
+  const args = message.content.split(" ").slice(1);
+ 
+ 
+ 
+  if (command == "kick") {
+  if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');        
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**انت لا تملك صلاحية للكيك**");
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**البوت لا يملك صلاحيات الكيك");
+  const user = message.mentions.users.first();
+  const reason = message.content.split(" ").slice(2).join(" ");
+if (message.mentions.users.size < 1) return message.reply("**منشن الشخص المراد طرده**");
+if (!message.guild.member(user)
+.kickable) return message.reply("**لايمكنني طرد هذا الشخص**");
+ 
+  message.guild.member(user).kick();
+ 
+  const kickembed = new Discord.RichEmbed()
+  .setAuthor(`تم طرد العضو`, user.displayAvatarURL)
+  .setColor("#502faf")
+  .setTimestamp()
+  .addField("**العضو اللي انطرد**",  '**[ ' + `${user.tag}` + ' ]**')
+  .addField("**العضو اللي قام بالطرد**", '**[ ' + `${message.author.tag}` + ' ]**')
+  .addField("**السبب**", '**[ ' + `${reason}` + ' ]**')
+  message.channel.send({
+    embed : kickembed
+   
+  })
+}
+});
+ 
+client.on('message', message => {
+  if (message.author.boss) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+var command = message.content.split(" ")[0];
+command = command.slice(prefix.length);
+ 
+  const args = message.content.split(" ").slice(1);
+ 
+  if (command == "ban") {
+      if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');        
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**انت لا تملك صلاحية الباند**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("البوت لايملك صلاحيات الباند");
+  const user = message.mentions.users.first();
+  const reason = message.content.split(" ").slice(2).join(" ");
+if (message.mentions.users.size < 1) return message.reply("**منشن الشخص اللي تريد تبنيده**");
+  if (!message.guild.member(user)
+.kickable) return message.reply("**لايمكنني تبنيد هذا الشخص**");
+ 
+  message.guild.member(user).ban();
+ 
+  const banembed = new Discord.RichEmbed()
+  .setAuthor(`تم تبنيد العضو`, user.displayAvatarURL)
+  .setColor("#502faf")
+  .setTimestamp()
+  .addField("**العضو الي تبند:**",  '**[ ' + `${user.tag}` + ' ]**')
+  .addField("**العضو اللي قام بتبنيده:**", '**[ ' + `${message.author.tag}` + ' ]**')
+  .addField("**السبب**", '**[ ' + `${reason}` + ' ]**')
+  message.channel.send({
+    embed : banembed
+  })
+}
+});
+
+
+//none
 client.login(process.env.BOT_TOKEN);

cd7c2134edfec62c249c8b6bbc5e0120485ac6c0
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-24 16:04:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2068,7 +2068,29 @@ client.on("guildCreate", guild => {
     });
 
 
-//كود البروفايل
+//كود يثبت لك البوت في روم صوتي
+
+client.on('message', message => {
+ if(message.content.startsWith(prefix + "تعال")) {
+message.member.voiceChannel.join();
+}
+});
+
+==============================
+client.on('message', msg => {
+
+    if (msg.content == '1join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('✅'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("493780860810690601").join();
+    });
 
 
 //none

715167c1f67b66ddfebc8e7b30f90ec6b67db52e
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-24 13:05:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1979,37 +1979,14 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
     }
 });
 
-//كود عمل سيرفر ويبعت لك رابطو
+//كود لما تمنشن البوت يرد عليكي
 
-client.on("message", message => {
-    var prefix = "*"
-    if(message.content.startsWith(prefix + "CreateGuild")) {
-client.user.createGuild('Example Guild', 'london').then(guild => {
-  guild.channels.get(guild.id).createInvite()
-    .then(invite => client.users.get('<USERID>').send(invite.url));
-  guild.createRole({name:'Example Role', permissions:['ADMINISTRATOR']})
-    .then(role => client.users.get('<UserId>').send(role.id))
-    .catch(error => console.log(error))
+client.on('message', message=> {
+    if (message.author.bot) return;
+    if (message.isMentioned(client.user))
+    {
+    message.reply("هلا امرني اذا تبي مساعده اكتب *help");
+    }
 });
-​
-/* ES8 async/await */
-async function createGuild(client, message) {
-  try {
-    const guild = await client.user.createGuild('Example Guild', 'london');
-    const defaultChannel = guild.channels.find(c=> c.permissionsFor(guild.me).has("SEND_MESSAGES"));
-    const invite = await defaultChannel.createInvite();
-    await message.author.send(invite.url);
-    const role = await guild.createRole({ name:'Example Role', permissions:['ADMINISTRATOR'] });
-    await message.author.send(role.id);
-  } catch (e) {
-    console.error(e);
-  }
-}
-createGuild(client, message);
-// Run this once you've joined the bot created guild.
-message.member.addRole('<THE ROLE ID YOU GET SENT>');
-
-}})
-
 //none
 client.login(process.env.BOT_TOKEN);

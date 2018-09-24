bf6fbfff0ba25531509831947b5b0fb7965f6b2b
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-24 11:53:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -943,6 +943,17 @@ client.on('message', async message => { // Alpha Codes Server.
 
 //كود البرودكاست البدون اي شي فقط الكلام
  
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('nbc')){
+ if(!message.author.id === '') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 //كود تغير البلاينج و الصوره والاسم
 

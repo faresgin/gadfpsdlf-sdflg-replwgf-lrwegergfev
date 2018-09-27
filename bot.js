b557b8114c9ff67c4e94d001c7390a2912db318e
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-27 21:27:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,17 +30,17 @@ client.on('ready', () => {
 //كود الرينبو السليم وامن
 
 client.on("message", message => {
- 
+
   function discoRole() {
     let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
     roles.forEach((role) => {
-      let theRole = message.guild.roles.find("Sec", role);
+      let theRole = message.guild.roles.find("name", role);
       theRole.edit({color: random}).catch(e => {
         return message.channel.send(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
       });
     });
   }
- 
+
   if(message.content.startsWith(prefix + "startdisco")) {
     if(allowedUsers.includes(message.author.id)) {
     setInterval(() => { discoRole(); }, config.ms);
@@ -50,7 +50,7 @@ client.on("message", message => {
     message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
   }
 } else
- 
+
 if(message.content.startsWith(prefix + "stopdisco")) {
   if(allowedUsers.includes(message.author.id)) {
   message.channel.send("I've stopped discoing.");
@@ -59,14 +59,14 @@ if(message.content.startsWith(prefix + "stopdisco")) {
   message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
   }
 }
- 
+
 });
 const config = require('config.json');
 const prefix = config.prefix;
- 
+
 const allowedUsers = config.allowedUsers;
 const roles = config.roleToDisco;
- 
+
 client.on("ready", () => {
     client.user.setPresence({ game: { name: `Disco Roles! Created by i am toast#1213` }, type: 0 });
     console.log("Disco role bot online! Created by i am toast.");

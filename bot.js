0377d8d6c1ed42874dfc87794bdd6cb8739f516f
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-28 01:34:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,6 +110,31 @@ https://discord.gg/FNqMbzm
       message.channel.send(embed)
       }
   });
+//كود يثبت البوت ب روم
+
+const Discord = require("discord.js")
+const client = new Discord.Client();
+const YTDL = require('ytdl-core');
+const nodeopus = require('node-opus');
+const ffmpeg = require('ffmpeg');
+var servers = {};
+function play(connection, message, args) {
+  var server = servers[message.guild.id];
+  server.dispatcher = connection.playStream(YTDL(args[0]), {filter: "audioonly"});
+  server.queue.shift();
+  server.dispatcher.on("end", function() {
+    if (server.queue[0]) play(connection, message);
+    else connection.disconnect();
+  });
+}
+
+
+client.on('message', message =>{
+  if(message.content.startsWith('join')){
+    const voiceChannel = message.member.voiceChannel
+    voiceChannel.join();
+    message.channel.send("تم الأتصال بالروم الصوتي")
+});
 
 //كود الانفيت بشكل جميل 
 

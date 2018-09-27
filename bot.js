5b90576c20ba55678b071f5e728958b3fe1403eb
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-27 22:28:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,51 +27,19 @@ client.on('ready', () => {
   console.log('')
 });
 
-//ren
+//كود اليخلي الروم ينكتب واحده واحده
 
-client.on("message", message => {
- 
-  function discoRole() {
-    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    roles.forEach((role) => {
-      let theRole = message.guild.roles.find("Sec", role);
-      theRole.edit({color: random}).catch(e => {
-        return message.channel.send(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
-      });
-    });
-  }
- 
-  if(message.content.startsWith(prefix + "startdisco")) {
-    if(allowedUsers.includes(message.author.id)) {
-    setInterval(() => { discoRole(); }, config.ms);
-    message.channel.send("```css\nDiscoing...```");
-    message.channel.send("Please make sure you read the README, you could get IP banned from discord because of ratelimits.");
-  } else {
-    message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
-  }
-} else
- 
-if(message.content.startsWith(prefix + "stopdisco")) {
-  if(allowedUsers.includes(message.author.id)) {
-  message.channel.send("I've stopped discoing.");
-  setTimeout(() => { console.log(process.exit(0)); }, 300);
-} else {
-  message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
-  }
-}
- 
-});
-
-const config = JSON.parse(fs.readFileSync("./config.json", "utf8")); 
-
-const allowedUsers = config.allowedUsers;
-const roles = config.roleToDisco;
- 
-client.on("ready", () => {
-    client.user.setPresence({ game: { name: `Disco Roles! Created by i am toast#1213` }, type: 0 });
-    console.log("Disco role bot online! Created by i am toast.");
-});
-
+client.on("ready", async  => {
+  setInterval(function(){
+  client.channels.find('id', '494557396656455730').setName("ر");
+  client.channels.find('id', '494557396656455730').setName("رو");
+  client.channels.find('id', '494557396656455730').setName("روم");
+  client.channels.find('id', '494557396656455730').setName("؏ روم ");
+  client.channels.find('id', '494557396656455730').setName("روم ּ؏ــٰ");
+  client.channels.find('id', '494557396656455730').setName("روم ּ؏ــٰا̍");
+  client.channels.find('id', '494557396656455730').setName("روم ּ؏ــٰا̍م");
+    }, 10000);
+  });
 //كود اليغير الحال حقت البوت كل شوي بس بلاينج
 
 client.on('ready', function(){

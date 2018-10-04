7e53ee4dc6743d1fa2d2fd1d08fd4b77a395aa96
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-10-04 19:44:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ const prefix = '*'
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(` Say *help || *inv `,"http://twitch.tv/Idk")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')

d910b07ea3df69e4d713a8784663527b53031f7c
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-10-17 21:28:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,7 +45,6 @@ client.on('ready', () => {
 //كود الكريدت
 
 const moment = require("moment");  
-const fs = require("fs");      
 const dateFormat = require('dateformat');
 const Canvas = require("canvas");
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))

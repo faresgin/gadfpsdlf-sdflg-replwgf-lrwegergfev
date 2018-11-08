1d0d52b46fe11c1caf7a8cbeb3c305bf5959d3b2
adadadadadfgfgfgfgfg <43391576+AhmedMagdyi@users.noreply.github.com>
2018-11-08 05:04:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -778,33 +778,7 @@ message.channel.send({embed:alpha});
 }
 });
 
-------------
 
-client.on('message', message => {
-    if (message.content === ('$bot') {
-    message.channel.send({
-        embed: new Discord.RichEmbed()
-            .setAuthor(client.user.username,client.user.avatarURL)
-            .setThumbnail(client.user.avatarURL)
-            .setColor('RANDOM')
-            .setTitle('Arabe Codes.')
-            .addField('**My Ping**' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
-            .addField('**RAM Usage**', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
-            .addField('**Servers**', [client.guilds.size], true)
-            .addField('**Channels**' , `[ ${client.channels.size} ]` , true)
-            .addField('**Users**' ,`[ ${client.users.size} ]` , true)
-            .addField('**My Name**' , `[ ${client.user.tag} ]` , true)
-            .addField('**My ID**' , `[ ${client.user.id} ]` , true)
-            .addField('**DiscordJS**' , `[ ${Discord.version} ]` , true)
-            .addField('**NodeJS**' , `[ ${process.version} ]` , true)
-            .addField('**Arch**' , `[ ${process.arch} ]` , true)
-            .addField('**Platform**' , `[ ${process.platform} ]` , true)
-                  .addField('**My Prefix**' , `[ ${prefix} ]` , true)
-                  .addField('**My Language**' , `[ Java Script ]` , true)
-                  .setFooter('By |!                  iiMostafaYT™#1001')
-    })
-}
-});
  
 //بعض الاكواد الاداريه اولهم كود مسح الشات
  

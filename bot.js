91ce1a80107c34f17fa61367302e10810cebd9cb
adadadadadfgfgfgfgfg <43391576+AhmedMagdyi@users.noreply.github.com>
2018-11-08 05:17:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ const prefix = '*'
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity(argresult , {type:'LISTENING'});
+  client.user.setActivity( GMZN , 'LISTENING');
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')

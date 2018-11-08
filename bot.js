0ed48ef1c98a350897ed33c37b8164cc036b6aaa
adadadadadfgfgfgfgfg <43391576+AhmedMagdyi@users.noreply.github.com>
2018-11-08 07:35:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,7 +48,7 @@ client.on("ready", () => {
   function arabecodes() {
     client.guilds.get('458387793975181314').roles.find("name", "DISCO").setColor("RANDOM");
   }
-  setInterval(arabecodes, 3000);
+  setInterval(arabecodes, 7000);
 })
 
 
